4ede600f6b16f3c8a4ff1979388225e92f29a40b
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-01-21 01:17:19 +0300

    boxOffice-finish

--- src/components/boxOffice/BoxOffice.jsx
+++ src/components/boxOffice/BoxOffice.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Glider from 'react-glider';
+
 import BoxOffice1 from "../../img/boxOffice/thumb-1.jpg"
 import BoxOffice2 from "../../img/boxOffice/thumb-2.jpg"
 import BoxOffice3 from "../../img/boxOffice/thumb-3.jpg"
 import BoxOffice4 from "../../img/boxOffice/thumb-4.jpg"
 import BoxOffice5 from "../../img/boxOffice/thumb-5.jpg"
+import Author from "../../img/boxOffice/author.png"
 import "../css/BoxOffice.css"
 function BoxOffice() {
   return (
@@ -18,7 +21,7 @@ function BoxOffice() {
                   <li>
                     <img src={BoxOffice1} alt='' />
                   </li>
-                  <li>1</li>
+                  <li className='mx-4'>1</li>
                   <li>
                     <h4>X-Men: Apocalypse</h4>
                     <span>Action, thriller</span>
@@ -33,7 +36,7 @@ function BoxOffice() {
                   <li>
                     <img src={BoxOffice2} alt='' />
                   </li>
-                  <li>2</li>
+                  <li className='mx-2'>2</li>
                   <li>
                     <h4> Warcraft</h4>
                     <span>Action, thriller</span>
@@ -48,7 +51,7 @@ function BoxOffice() {
                   <li>
                     <img src={BoxOffice3} alt='' />
                   </li>
-                  <li>3</li>
+                  <li className='mx-2'>3</li>
                   <li>
                     <h4>Hobbit: The battle
                     </h4>
@@ -64,7 +67,7 @@ function BoxOffice() {
                   <li>
                     <img src={BoxOffice4} alt='' />
                   </li>
-                  <li>4</li>
+                  <li className='mx-3'>4</li>
                   <li>
                     <h4> James Bond: Spectre
                     </h4>
@@ -93,7 +96,57 @@ function BoxOffice() {
             </ul>
           </div>
           <div className="col-sm-5 col-xs-12 ">
+            <h3>testimonials</h3>
+            <div className='testimontal'>
+              <Glider
 
+
+                className="glider-container"
+                draggable
+                hasDots
+                slidesToShow={1}
+                slidesToScroll={2}
+                scrollToSlide={5}
+              >
+                <div className='text-center'>
+                  <img src={Author} className='' alt='' />
+                  <div className="entry-meta">
+                    <h4>Shela Mathews</h4>
+
+                  </div>
+                  <p>
+
+                    Enthusiastically monetize plug-and-play scenarios through quality manufactured products. Monotonectally streamline standardized portals after proactive innovation. Energistically promote market positioning.
+
+                  </p>
+                </div>
+                <div className='text-center'>
+                  <img src={Author} className='' alt='' />
+                  <div className="entry-meta">
+                    <h4>Shela Mathews</h4>
+
+                  </div>
+                  <p>
+
+                    Enthusiastically monetize plug-and-play scenarios through quality manufactured products. Monotonectally streamline standardized portals after proactive innovation. Energistically promote market positioning.
+
+                  </p>
+                </div>
+                <div className='text-center'>
+                  <img src={Author} className='' alt='' />
+                  <div className="entry-meta">
+                    <h4>Shela Mathews</h4>
+
+                  </div>
+                  <p>
+
+                    Enthusiastically monetize plug-and-play scenarios through quality manufactured products. Monotonectally streamline standardized portals after proactive innovation. Energistically promote market positioning.
+
+                  </p>
+                </div>
+
+              </Glider>
+            </div>
           </div>
         </div>
       </div>
