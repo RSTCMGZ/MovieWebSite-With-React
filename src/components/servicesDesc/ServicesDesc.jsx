caceb7a0d0f4a40b05d60009400dbd32f2300c2e
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-01-20 23:42:23 +0300

    service-finish

--- src/components/servicesDesc/ServicesDesc.jsx
+++ src/components/servicesDesc/ServicesDesc.jsx
@@ -2,13 +2,60 @@ import React from 'react'
 import "../css/services-desc.css"
 function ServicesDesc() {
     return (
-        <div className='mt-5 Services-Desc mb-5'>
-            <div className="row">
-                <div className="col-sm-12">
-                    <h3 className='text-center '>YOUR EXPERIENCE IS GONNA BE EXQUISITE.
-                    </h3>
+        <div className=' Services-Desc'>
+            <div className="container service">
+
+                <div className="row">
+                    <div className="col-sm-12">
+                        <h3 className='text-center mt-5'>YOUR EXPERIENCE IS GONNA BE EXQUISITE.
+                        </h3>
+                    </div>
+
                 </div>
+                <div className="row mt-5">
+                    <div className="col-md-4 col-sm-12">
+                        <h2>IMAX®</h2>
+                        <p> IMAX® provides an exceptional movie experience with images of immersive size, striking clarity.</p>
+                    </div>
+                    <div className="col-md-4 col-sm-12">
+                        <h2>REALD™ 3D
+                        </h2>
+                        <p>RealD™ Digital 3D is the new generation of entertainment, with crisp, bright, ultra-realistic images.
+
+                        </p>
+                    </div>
+                    <div className="col-md-4 col-sm-12">
+                        <h2>DOLBY® LABORATORIES
+                        </h2>
+                        <p> (NYSE:DLB) is the global leader in technologies that are essential elements in the best entertainment.</p>
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-md-4 col-sm-12">
+                        <h2>GREAT ENVIRONMENT
+                        </h2>
+                        <p> IMAX® provides an exceptional movie experience with images of immersive size, striking clarity.</p>
+                    </div>
+                    <div className="col-md-4 col-sm-12">
+                        <h2>ENTERTAINMENT CENTER
+                        </h2>
+                        <p> RealD™ Digital 3D is the new generation of entertainment, with crisp, bright, ultra-realistic images.
+
+
+
+                        </p>
+                    </div>
+                    <div className="col-md-4 col-sm-12">
+                        <h2>SO MUCH MORE...
+                        </h2>
+                        <p> (NYSE:DLB) is the global leader in technologies that are essential elements in the best entertainment.
+
+                            .</p>
+                    </div>
+                </div>
+
             </div>
+
         </div>
     )
 }
