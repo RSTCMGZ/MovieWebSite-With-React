0b428147fa236fbfb8f7d1a0cdf39316a3533e78
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-01-20 23:28:15 +0300

    start-services

--- src/components/servicesDesc/ServicesDesc.jsx
+++ src/components/servicesDesc/ServicesDesc.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import "../css/services-desc.css"
 function ServicesDesc() {
     return (
-        <div className='mt-5'>
-            Hello
+        <div className='mt-5 Services-Desc mb-5'>
+            <div className="row">
+                <div className="col-sm-12">
+                    <h3 className='text-center '>YOUR EXPERIENCE IS GONNA BE EXQUISITE.
+                    </h3>
+                </div>
+            </div>
         </div>
     )
 }
